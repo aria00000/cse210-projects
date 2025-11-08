6b784f515721718bb2a4eb811b6ebb330e7309b9
Aria Takahashi <ariatakahashi@Arias-MacBook-Air.local>
2025-11-07 21:28:57 -0700

    Guess My Number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string guess = "false";
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        //Console.WriteLine($"Magic Number is: {magicNumber}");
+
+        while (guess == "false")
+        {
+
+            Console.WriteLine("What is your guess?");
+            string userGuessInput = Console.ReadLine();
+            int userGuess = int.Parse(userGuessInput);
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guess = "true";
+            }
+        }
+
+
     }
 }
