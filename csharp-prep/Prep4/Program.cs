ab2e7613d76b2c1556d541fc42b0ec1e03be89fa
Aria Takahashi <ariatakahashi@Arias-MacBook-Air.local>
2025-11-07 21:47:09 -0700

    Ask the user for a series of numbers, and append each one to a list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("  Enter a list of numbers, type 0 when finished.");
+        int sum = 0;
+        int average = 0;
+        int numberInput = -1;
+        List<int> numbers = new List<int>();
+
+        while (numberInput != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            numberInput = number;
+            numbers.Add(number);
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        average = sum / (numbers.Count - 1);
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
