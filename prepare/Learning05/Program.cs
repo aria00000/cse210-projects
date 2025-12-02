ed56b13e627c96c1601eba8a00f2b05468aab9dc
Aria Takahashi <ariatakahashi@Arias-MacBook-Air.local>
2025-12-02 09:22:12 -0700

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 4);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Green", 5, 6);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Blue", 3);;
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Shape Color: {color}, Area: {area}");
+        }
+    
     }
 }
